9b672bda93d000293b4274b8f5f5dd66db1d6d17
Andrei Kolotilo <andrei.kolotilo@netcracker.com>
2019-12-24 22:47:27 +0300

    Add init method

--- src/main/java/SiteTestAutomation/Elements/complexElements/ComplexElement.java
+++ src/main/java/SiteTestAutomation/Elements/complexElements/ComplexElement.java
@@ -1,10 +1,13 @@
-package SiteTestAutomation.Elements.Clases;
+package SiteTestAutomation.Elements.complexElements;
 
+import SiteTestAutomation.Elements.Clases.BaseElement;
 import SiteTestAutomation.Pages.Interfaces.Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class ComplexElement extends BaseElement implements SearchContext {
@@ -22,5 +25,10 @@ public class ComplexElement extends BaseElement implements SearchContext {
 
     public WebElement findElement(By by) {
         return getWrappedElement().findElement(by);
+    }
+
+    protected <T> T initElement(Class<T> clazz, By by) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<T> constr = clazz.getConstructor(Page.class, SearchContext.class ,By.class );
+        return constr.newInstance(this.hostPage, this, by);
     }
 }
