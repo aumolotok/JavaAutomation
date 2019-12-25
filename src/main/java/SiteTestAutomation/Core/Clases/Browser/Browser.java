32ee92f0018df93c2c279ca5fa437f8fe7e32e7f
Andrei Kolotilo <andrei.kolotilo@netcracker.com>
2019-12-26 01:48:11 +0300

    Add new browser methods

--- src/main/java/SiteTestAutomation/Core/Clases/Browser/Browser.java
+++ src/main/java/SiteTestAutomation/Core/Clases/Browser/Browser.java
@@ -1,21 +1,26 @@
 package SiteTestAutomation.Core.Clases.Browser;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Browser{
     private WebDriver driver;
 
     public Browser(WebDriver driver){
         this.driver = driver;
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     public Browser(){
-        this.driver = new ChromeDriver();
+        this(new ChromeDriver());
     }
 
     public void goToUrl(String url){
@@ -23,8 +28,38 @@ public class Browser{
     }
 
     public WebElement findElement(By by){
-        return driver.findElement(by);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        return wait.until((driver) -> driver.findElement(by));
     }
 
     public List<WebElement> findElements(By by) {return driver.findElements(by);}
+
+    public void browserWait(long value,TimeUnit timeDemention){
+        WebDriverWait wait = new WebDriverWait(driver, value);
+        wait.withTimeout(value, timeDemention);
+    }
+
+    public void waitForDisappear(int seconds, By by){
+        WebDriverWait wait = new WebDriverWait(driver, seconds);
+        wait.until((driver) -> driver.findElements(by).size() == 0  || driver.findElement(by).isDisplayed() == false);
+    }
+
+    public void scrollToElement(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public void scrollUp(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
+    }
+
+    public void scrollDown(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+    }
+
+
+
+    //window.scrollTo(document.body.scrollHeight,0)
 }
