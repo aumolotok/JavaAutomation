68e5a18824613e08d3115ff85d1217a339a66ada
Andrei Kolotilo <andrei.kolotilo@netcracker.com>
2019-12-26 01:49:23 +0300

    Implement LinkElement interface in Button class

--- src/main/java/SiteTestAutomation/Elements/Clases/Button.java
+++ src/main/java/SiteTestAutomation/Elements/Clases/Button.java
@@ -1,10 +1,11 @@
 package SiteTestAutomation.Elements.Clases;
 
+import SiteTestAutomation.Elements.Interfaces.LinkElement;
 import SiteTestAutomation.Pages.Interfaces.Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 
-public class Button extends BaseElement {
+public class Button extends BaseElement implements LinkElement {
 
     public void click(){ getWrappedElement().click();}
 
@@ -14,5 +15,10 @@ public class Button extends BaseElement {
 
     public Button(Page hostPage, SearchContext parent, By by) {
         super(hostPage, parent, by);
+    }
+
+    @Override
+    public String getInnerLink() {
+        return getWrappedElement().findElement(By.xpath(".//a")).getAttribute("href");
     }
 }
