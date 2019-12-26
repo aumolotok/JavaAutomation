09b126a456e8a6e8c2b4080773ddbbe1a930dfc0
Andrei Kolotilo <andrei.kolotilo@netcracker.com>
2019-12-26 19:32:27 +0300

    Add test body

--- src/test/java/SiteTestAutomation/Pages/Clases/MainPageTest.java
+++ src/test/java/SiteTestAutomation/Pages/Clases/MainPageTest.java
@@ -1,52 +1,42 @@
 package SiteTestAutomation.Pages.Clases;
 
-import SiteTestAutomation.Core.Clases.Browser.Browser;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.Description;
+import SiteTestAutomation.Core.Clases.Browser.PageFacility;
+import SiteTestAutomation.Utils.StringUtils;
+import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+
 import java.lang.reflect.InvocationTargetException;
 
-
-public class MainPageTest {
-
-    private static Browser browser;
-
-    @BeforeClass
-    public static void setUp(){
-        //System.setProperty("webdriver.chrome.driver", "D:\List_of_Jar\chromedriver.exe");
-        WebDriverManager.getInstance(ChromeDriver.class).setup("79.0.3945.36");
-        browser = new Browser(new ChromeDriver());
-        browser.goToUrl("https://www.wrike.com/");
-    }
-
+@Epic("Wrike trials")
+@Feature("Wrike trial mail workflow")
+public class MainPageTest extends BaseTest{
 
     @Test
     @DisplayName("Check Trial Submit and Tweeter Button")
-    @Description("Extremely useful description of testing test ")
+    @Description("Extremely useful description of testing test")
+    @Severity(SeverityLevel.CRITICAL)
     public void clickButton() throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, InterruptedException {
-        MainPage main = new MainPage(browser);
-        main.clickStartTrialHeaderButton();
+        String email = StringUtils.generateString( 10) + "+wpt@wriketask.qaa";
 
-        StartTrialPage trialPage = main.sentEmailForTrial();
+        MainPage main = PageFacility.openPageByPageUrl(MainPage.class, browser);
+        main.clickStartTrialHeaderButton();
+        StartTrialPage trialPage = main.sentEmailForTrial(email);
+
+        Assert.assertEquals(trialPage.getCurrentUrl(), trialPage.getPatternUrl());
+        Assert.assertTrue(trialPage.getMessageWithEmail().contains("email"));
+
         trialPage.fillSurvey();
         trialPage.submitSurvey();
         trialPage.scrollUp();
 
-        Assert.assertEquals("Thanks for helping us out!" , trialPage.getSuccessMessage());
+        Assert.assertEquals("Thanks for helping us out!", trialPage.getSuccessMessage());
 
         trialPage.scrollDown();
 
-        String xlink = trialPage.footer.tweeterButton.getSourseImage();
-
-        Assert.assertEquals("/content/themes/wrike/dist/img/sprite/vector/footer-icons.symbol.svg?v2#twitter", xlink);
-
-        String link = trialPage.footer.tweeterButton.getInnerLink();
-
-        Assert.assertEquals("https://twitter.com/wrike", link);
-
+        Assert.assertEquals("/content/themes/wrike/dist/img/sprite/vector/footer-icons.symbol.svg?v2#twitter", trialPage.footer.tweeterButton.getSourseImage());
+        Assert.assertEquals("https://twitter.com/wrike", trialPage.footer.tweeterButton.getInnerLink());
     }
 }
